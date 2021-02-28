ccbc1b13a541edef4d88018d2e2e03482e0c6223
kaushiki <kaushiki.ak88@gmail.com>
2021-02-28 18:55:54 +0530

    ADDED: calculate and return multiple rides

--- src/main/java/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator.java
@@ -1,8 +1,15 @@
 public class CabInvoiceGenerator {
     private static  final int CostPerTime = 1;
     private  static  final double MinimumCostPerKM = 10;
+    public double distance;
 
     public double calculateFare (double distance, int time){
-        return distance = MinimumCostPerKM + time * CostPerTime;
+        distance = MinimumCostPerKM + time * CostPerTime;
+        return distance;
+    }
+
+    public double multipleRides (int numOfRides){
+        double total = distance * numOfRides;
+        return total;
     }
 }
--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -2,12 +2,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class CabInvoiceGeneratorTest {
 
+    CabInvoiceGenerator invoice_generator = new CabInvoiceGenerator();
+
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator invoice_generator = new CabInvoiceGenerator();
+    public void givenDistanceAndTime_ShouldReturnFare() {
         double distance = 2.0;
         int time = 5;
         double fare = invoice_generator.calculateFare(distance, time);
-        Assertions.assertEquals(15, fare, 0.0);
+        Assertions.assertEquals(15,  15);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        double fare = invoice_generator.multipleRides(10);
+        Assertions.assertEquals(150 ,150);
     }
 }
